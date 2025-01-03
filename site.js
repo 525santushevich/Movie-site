7291e8ead65d98e778050e367c61dcbbf4cd3cfc
525santushevich <525santushevich@frhsd.com>
2025-01-03 11:55:25 -0500

    step 8 idek

--- site.js
+++ site.js
@@ -47,45 +47,46 @@ const vue_app = Vue.createApp({
       switch (datesArray[1]) {
         case 1:
           month = "January";
-
+          break;
         case 2:
           month = "February";
-
+          break;
         case 3:
           month = "March";
-
+          break;
         case 4:
           month = "April";
-
+          break;
         case 5:
           month = "May";
-
+          break;
         case 6:
           month = "June";
-
+          break;
         case 7:
           month = "July";
-
+          break;
         case 8:
           month = "August";
-
+          break;
         case 9:
           month = "September";
-
+          break;
         case 10:
           month = "October";
-
+          break;
         case 11:
           month = "November";
-
+          break;
         case 12:
           month = "December";
+          break;
       }
-      return month + "" + datesArray[2] + "," + datesArray[0];
+      return month + " " + dateArray[2] + ", " + dateArray[0];
     },
     posterClick(index) {
       if (
-        this.movies[index].posterindex >
+        this.movies[index].posterindex <
         this.movies[index].posters.length - 1
       ) {
         this.movies[index].posterindex++;
@@ -93,10 +94,10 @@ const vue_app = Vue.createApp({
         this.movies[index].posterindex = 0;
       }
     },
-    timesText(minutes) {
+    timeText(minutes) {
       const hours = Math.trunc(minutes / 60);
-      const min = minutes - hours * 60;
-      return `${hours}h ${min}m`;
+      const mins = minutes - hours * 60;
+      return `${hours}h ${mins}m`;
     },
   },
 });
